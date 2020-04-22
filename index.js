71f1aa9848b186f82e5bff86f707ba9b3b5d73ca
Lio Fleishman <lio.fleishman@sisense.com>
2020-04-22 14:21:58 -0400

    adding index

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const app = express()
-app.get('/', (req, res) => {
-  res.send('HEY again!')
-})
+
+app.use(express.static('public'))
 app.listen(4000, () => console.log('Server running on port 4000'))
