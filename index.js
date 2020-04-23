72140208a30bb55d6eb88c099cf2f4ea41e9f40d
Lio Fleishman <lio.fleishman@sisense.com>
2020-04-22 21:52:24 -0400

    get again

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
-app.post('/',(req, res)=> {
-  app.send('hey hey hey!')
-})
 app.use(express.static('dist'))
+  app.post('/',(req, res)=> {
+    app.send('hey hey hey!')
+  })
 app.listen(4000, () => console.log('Server running on port 4000'))
