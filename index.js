9ef737a5bd2793a87b3832ac1c473bf148e60f33
Lio Fleishman <lio.fleishman@sisense.com>
2020-04-22 21:57:36 -0400

    changes here aws

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 app.use(express.static('dist'))
   app.get('/aws',(req, res)=> {
-    app.send('aws here...!')
+    res.send('aws here...!')
   })
 app.listen(4000, () => console.log('Server running on port 4000'))
