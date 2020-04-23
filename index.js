1b69620e58366e2860d11a3b3692bc2038d7d478
Lio Fleishman <lio.fleishman@sisense.com>
2020-04-22 21:16:51 -0400

    adding webpack and react

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express')
 const app = express()
 
-app.use(express.static('public'))
+app.use(express.static('dist'))
 app.listen(4000, () => console.log('Server running on port 4000'))
